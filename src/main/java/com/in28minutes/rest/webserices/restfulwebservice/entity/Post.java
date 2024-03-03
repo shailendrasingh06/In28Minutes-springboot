f8db9ecff00779c2859a86e99c4c502400410431
shailendrasingh06 <49592637+shailendrasingh06@users.noreply.github.com>
2024-03-04 01:02:32 +0530

    using mysql instead of h2 as a database

--- src/main/java/com/in28minutes/rest/webserices/restfulwebservice/entity/Post.java
+++ src/main/java/com/in28minutes/rest/webserices/restfulwebservice/entity/Post.java
@@ -7,7 +7,6 @@ import jakarta.persistence.*;
 public class Post {
 
     @Id
-    @GeneratedValue
     private Integer Id;
 
     private String description;
--- src/main/java/com/in28minutes/rest/webserices/restfulwebservice/entity/User.java
+++ src/main/java/com/in28minutes/rest/webserices/restfulwebservice/entity/User.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Entity(name = "user_details")
 public class User {
     @Id
-    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
